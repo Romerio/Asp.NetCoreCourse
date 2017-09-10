a2b6b45961d3df86bedd9559e9225c2cd8cac674
Romério Sousa <romerio.unix@gmail.com>
2017-09-10 11:57:29 -0300

    fixed untracked files

--- src/StoreOfBuild.Web/Controllers/CategoryController.cs
+++ src/StoreOfBuild.Web/Controllers/CategoryController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StoreOfBuild.Domain.Dtos;
+using StoreOfBuild.Domain.Products;
 
 namespace StoreOfBuild.Web.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly CategoryStorer _categoryStorer;
+
+        public CategoryController(CategoryStorer categoryStorer)
+        {
+            _categoryStorer = categoryStorer;
+        }
+        
         public IActionResult Index()
         {
             return View();
@@ -20,7 +29,7 @@ namespace StoreOfBuild.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult CreateOrEdit(int id)
+        public IActionResult CreateOrEdit(CategoryDto dto)
         {
             return View();
         }
